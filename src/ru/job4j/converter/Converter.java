44220da8ad81857db9d8f26775c718f6e7ee2bff
DeceBeLL74 <pimenovvv888@gmail.com>
2021-03-12 22:42:45 +0700

    9.2. Checkstyle [#493224]

--- src/ru/job4j/converter/Converter.java
+++ src/ru/job4j/converter/Converter.java
@@ -6,13 +6,13 @@ public class Converter {
     }
 
     public static float rubleToDollar(int value) {
-        return value/60;
+        return value / 60;
     }
 
     public static void main(String[] args) {
         int value = 400;
         float euro = Converter.rubleToEuro(value);
         float dollar = Converter.rubleToDollar(value);
-        System.out.println(value+ " rubles are " + euro + " euro, or "+ dollar + " dollar.");
+        System.out.println(value + " rubles are " + euro + " euro, or " + dollar + " dollar.");
     }
 }
--- src/ru/job4j/ru/job4j/calculator/ArgMethod.java
+++ src/ru/job4j/ru/job4j/calculator/ArgMethod.java
@@ -3,16 +3,14 @@ package ru.job4j.ru.job4j.calculator;
 public class ArgMethod {
     public static void hello(String name, int age) {
         System.out.println("Hello, " + name);
+
     }
 
     public static void main(String[] args) {
         String name = "Petr Arsentev";
         int age = 33;
-
         ArgMethod.hello(name, age);
         System.out.println("А вообще, не очень понятно, что нужно сделать");
 
     }
-
-
 }
